f82519eaf8957bad17a60d4f885683dbd40a0372
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-30 12:11:26 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 // Database configuration
-$db_host = "sql11.freemysqlhosting.net"; // Server: Hostname of the database server
-$db_username = "sql11695074"; // Username: Username for database access
-$db_password = "8aKsudJmLp"; // Password: Password for database access
-$db_name = "sql11695074"; // Name: Name of the database (without file extension)
-$db_port = "3306"; // Port number: Port number for the database connection
+$db_host = ""; // Server: Hostname of the database server
+$db_username = ""; // Username: Username for database access
+$db_password = ""; // Password: Password for database access
+$db_name = ""; // Name: Name of the database (without file extension)
+$db_port = ""; // Port number: Port number for the database connection
 
 // Attempt to establish a connection to the database
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name, $db_port);
